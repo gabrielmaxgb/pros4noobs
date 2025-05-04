3a14cb7b13329b60482257d9e93b6bcc81a65543
gabrielmaxgb <gmaxgomes@gmail.com>
2025-05-03 23:49:17 -0300

    ++

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,26 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
+  fonts: {
+    families: [
+      { name: 'Chakra Petch', provider: 'google' },
+      { name: 'Gloock', provider: 'google' },
+      { name: 'JetBrains Mono', provider: 'google' },
+      { name: 'Staatliches', provider: 'google' },
+      { name: 'Orbitron', provider: 'google' }
+    ]
+  },
 
   modules: [
     '@nuxt/eslint',
