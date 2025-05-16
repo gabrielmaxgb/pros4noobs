540f732c6ea10004eb0ab3483e83af4ba291db12
titosilva <jt.mat@hotmail.com>
2025-05-16 08:34:48 -0300

    feat: adds argon2id for password hashing

--- server/api/user/create.post.ts
+++ server/api/user/create.post.ts
@@ -8,6 +8,7 @@ import {
   // type TUserRole,
   createUserSchema,
 } from '~/shared/user';
+import * as argon2 from 'argon2';
 
 export default defineEventHandler(async (event) => {
   try {
@@ -38,11 +39,20 @@ export default defineEventHandler(async (event) => {
       return { status: 400, message: 'Invalid technology selected.' };
     }
 
+    const passwordHash = await argon2.hash(data.password, {
+      type: argon2.argon2id, // Uses Argon2id for resistance to side-channel attacks
+      memoryCost: 2 ** 16,   // 64 MB of memory (standard for Argon2)
+      timeCost: 3,           // Iterations (3 is generally considered secure)
+      parallelism: 1,        // Number of threads (1 for server-side hashing)
+      hashLength: 32,        // Length of the resulting hash
+    });
+
     // Create a new user
     const newUser = new User({
       name: data.name,
       email: data.email,
       password: data.password, // Note: Hash the password in production
+      passwordHash: passwordHash,
       technologies: data.technologies,
       // initialRoles: body.initialRoles,
       initialRole: data.startRole,
@@ -58,10 +68,10 @@ export default defineEventHandler(async (event) => {
       technologies: newUser.technologies,
       initialRole: newUser.initialRole,
       superNoob: newUser.startedAsSuperBeginner,
-      // initialRoles: newUser.initialRoles.map((role: string) => role as UserRole),
+      roles: newUser.roles.map((role: string) => role as 'noob' | 'pro'),
     };
 
-    return { status: 201, message: 'User created successfully.', user: newUser };
+    return { status: 201, message: 'User created successfully.', user: model };
   } catch (error: any) {
     return { status: 500, message: 'Internal server error.', error: error?.message };
   }
